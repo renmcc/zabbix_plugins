6e30973b184f63cf20ff90a4dd88e96af309c2f9
ren_mcc <ren_mcc@foxmail.com>
2018-11-07 09:37:44 +0800

    微信公众号报警

--- wechat.sh
+++ wechat.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-CropID='wx8940da86ae63b94f'
+CropID='wx8940da86ae63b9'
 Secret='z_wlGexCrjmg_lsCqRYvZxNQvK5tahDlAje3ye_HFddjcIn6hTuAwFcc5gIuj9_G'
-GURL="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$CropID&corpsecret=$Secret" 
+GURL="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$CropID&corpsecret=$Secret"
 Gtoken=$(/usr/bin/curl -s -G "$GURL"|awk -F'"' '{print $4}')
 PURL="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=$Gtoken"
 
 function body() {
-        local int AppID=1            
-        local UserID=$1             
-        local PartyID=1            
+        local int AppID=1
+        local UserID=$1
+        local PartyID=1
         local Msg=$(echo "$@" | cut -d" " -f3-)
         printf '{\n'
         printf '\t"touser": "'"$UserID"\"",\n"
